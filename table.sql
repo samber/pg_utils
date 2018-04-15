67dc25f566dbcb39859fe2be8f0e0a4a46180d7e
Samuel BERTHE ツ <dev@samuel-berthe.fr>
2018-04-15 17:46:44 +0200

    Adding vacuum and analyze logs

--- table.sql
+++ table.sql
@@ -7,3 +7,21 @@ CREATE OR REPLACE VIEW my_show_table_size AS
     pg_size_pretty(pg_total_relation_size(relid)) As "Size",
     pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid)) as "External Size"
     FROM pg_catalog.pg_statio_user_tables ORDER BY pg_total_relation_size(relid) DESC;
+
+-- select * from my_show_table_vacuum('users');
+CREATE OR REPLACE FUNCTION my_show_table_vacuum(relname text) RETURNS TABLE(schemaname name, relname name, last_vacuum timestamp with time zone, last_autovacuum timestamp with time zone, vacuum_count bigint, autovacuum_count bigint) AS
+$body$
+  SELECT schemaname, relname, last_vacuum, last_autovacuum, vacuum_count, autovacuum_count
+    FROM pg_stat_all_tables
+    WHERE relname = $1
+$body$
+language sql;
+
+-- select * from my_show_table_analyze('users');
+CREATE OR REPLACE FUNCTION my_show_table_analyze(relname text) RETURNS TABLE(schemaname name, relname name, last_analyze timestamp with time zone, last_autoanalyze timestamp with time zone, analyze_count bigint, autoanalyze_count bigint) AS
+$body$
+  SELECT schemaname, relname, last_analyze, last_autoanalyze, analyze_count, autoanalyze_count
+    FROM pg_stat_all_tables
+    WHERE relname = $1
+$body$
+language sql;
