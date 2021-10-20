58a92631f291e8bf8684e77d7e74802ec39b6ddf
Samuel Berthe <dev@samuel-berthe.fr>
2021-10-20 12:01:27 +0200

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,12 +1,16 @@
 
--- select * from my_show_table_size;
-CREATE OR REPLACE VIEW my_show_table_size AS
+-- select * from my_show_table_size();
+CREATE OR REPLACE FUNCTION my_show_table_size() RETURNS TABLE(relname name, index_size text, size text, external_size text) AS
+$body$
   SELECT
     relname as "Table",
     pg_size_pretty(pg_indexes_size(relid)) AS "Index",
     pg_size_pretty(pg_total_relation_size(relid)) As "Size",
     pg_size_pretty(pg_total_relation_size(relid) - pg_relation_size(relid)) as "External Size"
-    FROM pg_catalog.pg_statio_user_tables ORDER BY pg_total_relation_size(relid) DESC;
+    FROM pg_catalog.pg_statio_user_tables ORDER BY pg_total_relation_size(relid) DESC
+$body$
+language sql;
+
 
 -- select * from my_show_table_vacuum('users');
 CREATE OR REPLACE FUNCTION my_show_table_vacuum(relname text) RETURNS TABLE(schemaname name, relname name, last_vacuum timestamp with time zone, last_autovacuum timestamp with time zone, vacuum_count bigint, autovacuum_count bigint) AS
@@ -16,6 +20,7 @@ $body$
     WHERE relname = $1
 $body$
 language sql;
+
 
 -- select * from my_show_table_analyze('users');
 CREATE OR REPLACE FUNCTION my_show_table_analyze(relname text) RETURNS TABLE(schemaname name, relname name, last_analyze timestamp with time zone, last_autoanalyze timestamp with time zone, analyze_count bigint, autoanalyze_count bigint) AS
